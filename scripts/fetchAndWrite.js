9c57bf1f16cad3c820df01c486a59340955dfac9
Mar.io <mario@stake.co.jp>
2024-03-21 18:02:47 +0100

    added for all contracts

---
+++ scripts/fetchAndWrite.js
@@ -0,0 +1,75 @@
+const axios = require("axios");
+const fs = require("fs");
+const { Parser } = require("json2csv");
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const fetchAndWrite = async (
+  contractAddress,
+  omaNumber,
+  compensateOma,
+  fileName,
+  block_gt,
+  block_lt
+) => {
+  let totalResults = 0;
+  let csvData = [];
+  const GRAPH_ENDPOINT =
+    "https://api.studio.thegraph.com/proxy/64002/yokidispatch/version/latest";
+
+  const fetchPage = async (skip) => {
+    const response = await axios.post(GRAPH_ENDPOINT, {
+      query: `
+                query MyQuery($first: Int, $skip: Int) {
+                    multiMinteds(
+                            first: $first,
+                            skip: $skip,
+                            where: {
+                                partnerNftContract: "${contractAddress}",
+                                omaAmount: "${omaNumber}"
+                                blockNumber_gt: "${block_gt}"
+                                blockNumber_lte: "${block_lt}"
+                            }
+                        ) {
+                            to
+                    }
+                }
+            `,
+      variables: {
+        first: 100,
+        skip: skip,
+      },
+    });
+
+    if (response.data != undefined && response.data.data != undefined) {
+    //   console.log(`fetching page ${skip / 100} ${response.data.data.length}`);
+      const mintedData = response.data.data.multiMinteds;
+    //   console.log(mintedData);
+      totalResults += mintedData.length;
+      csvData = csvData.concat(mintedData);
+    //   console.log(`total (${totalResults}), in page ${mintedData.length}`);
+
+      if (mintedData.length > 0) {
+        await delay(200); // wait for 1 second
+        await fetchPage(skip + 100);
+      }
+    } else {
+      console.log("No more data to fetch.");
+    }
+  };
+
+  await fetchPage(0);
+  console.log(
+    `(${totalResults}) ${fileName}:issued ${omaNumber} OMA, compensate ${compensateOma} OMA`
+  );
+  csvData = csvData.map((item) => [item.to, compensateOma]);
+  const json2csvParser = new Parser({ header: false });
+
+  const csv = json2csvParser.parse(csvData);
+
+  fs.writeFile(`output/${fileName}.csv`, csv, function (err) {
+    if (err) throw err;
+  });
+};
+
+module.exports = fetchAndWrite;
---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+const fetchAndWrite = require('./fetchAndWrite');
+
+fetchAndWrite("0x0CFd649E37882944342Ec0f8A2ac8ABca01f6934", 1, 4, "onigiriman1", 0, 500000);
+fetchAndWrite("0x0CFd649E37882944342Ec0f8A2ac8ABca01f6934", 3, 2, "onigiriman3", 0, 500000);
+fetchAndWrite("0x682cD0890C9c3aC423D534a50a96E9C6EF9983D2", 1, 4, "sashimi1", 0, 500000);
+fetchAndWrite("0x682cD0890C9c3aC423D534a50a96E9C6EF9983D2", 3, 2, "sashimi3", 0, 500000);
+fetchAndWrite("0xe3A4dBF34e21C7D24F86ef032A3E3beC39220B86", 0, 1, "candygirl0", 0, 500000);
+fetchAndWrite("0xAd61e3700A9ABa6dC5B986Ec39604167db33Fe09", 0, 1, "japCreator0-1", 0, 420000);
+fetchAndWrite("0xAd61e3700A9ABa6dC5B986Ec39604167db33Fe09", 0, 1, "japCreator0-2", 420000, 500000);
+fetchAndWrite("0xC384342Ad561B6791F4E38dE7A5e8b7747Bca5eE", 1, 1, "uzno1", 0, 500000);
